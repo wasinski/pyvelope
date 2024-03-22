f833c7a565ae68afe6438b3df92a807fad9684a6
twasinski <t.wasinski@dook.pro>
2024-03-22 21:22:00 +0100

    chore: move some docstrings

---
+++ src/pyvelope/_pyvelope/implementations/sqs/transport.py
@@ -0,0 +1,24 @@
+from typing import Protocol
+
+from pyvelope._pyvelope.abstractions.message_bus import Consumer, QueueRouter
+
+
+class SqsTransport(QueueRouter):
+    def bind_consumer(self, consumer_type: type[Consumer]) -> None:
+        """Bind a consumer to the transport.
+
+        This means that this transport will be used to deliver messages to this consumer,
+        based on the bound message type.
+
+        Method can be used when a consumer is part of the same codebase as the producer,
+        thus can be directly referenced.
+        """
+
+    def bind_msg_type(self, msg_type: type[object]) -> None:
+        """Bind a message type to this transport.
+
+        This means that this transport will be used to deliver messages of this type to
+        consumers that await this message type.
+
+        Method can be used when a consumer is not part of the same codebase as the producer.
+        """
